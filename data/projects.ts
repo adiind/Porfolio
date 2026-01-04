3e839cb9720b25b03ede8125fc806e402b0fe8fb
Aditya Agarwal <kriitya@gmail.com>
2026-01-04 01:34:04 -0800

    ADDED PROJECTS IMCOMPLETE

--- data/projects.ts
+++ data/projects.ts
@@ -7,12 +7,18 @@ import { Project } from '../types/Project';
 
 // Import project JSON files
 import suryaData from './projects/surya.json';
+import solopumpData from './projects/solopump.json';
+import heliosData from './projects/helios.json';
+import jarvisData from './projects/jarvis.json';
+import plotterData from './projects/plotter.json';
 
-// Type assertion and validation
-const projects: Project[] = [
+// Combine all projects
+export const PROJECTS: Project[] = [
     suryaData as Project,
+    solopumpData as Project,
+    heliosData as Project,
+    jarvisData as Project,
+    plotterData as Project,
 ];
 
-export const PROJECTS = projects;
-
-export default projects;
+export default PROJECTS;
